8194002605bf51525e187d4feae4f4bfaa68a8a3
A-ct-ive <yukkuriyusuke0418@gmail.com>
2021-08-24 10:00:26 +0900

    APPの練習

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <div>templateでは、HTMLとVue構文を書く。</div>
+    <div>{{ hello }}</div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -7,11 +9,16 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-
+console.log("scriptでは、JSを書く");
 export default {
   name: "App",
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      hello: "export default {} で Vueを書く。",
+    };
   },
 };
 </script>
@@ -24,5 +31,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app::after {
+  content: "styleではcssを書く";
 }
 </style>
